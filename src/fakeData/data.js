11f75277e349e21f95e5709ad58c71dae6953613
kawsar-95 <virtualkawsar@gmail.com>
2020-08-27 23:44:31 +0600

    done

---
+++ src/fakeData/data.js
@@ -0,0 +1,172 @@
+var data =[
+
+    {
+      "courseName":"Mastering React",
+      "instructor":"Mosh Hamedani",
+      "duration":"4 Months",
+      "picture":"https://i.ibb.co/D9QTLdK/courses-online-1.jpg",
+      "id":1,
+      "price":57
+
+    },
+
+
+{
+      "courseName":"The Ultimate Design Patterns Series",
+      "instructor":"Mosh Hamedani",
+      "duration":"6 Months",
+      "picture":"https://i.ibb.co/jhK780n/courses-online-1.png",
+      "id":2,
+      "price":87
+
+    },
+    
+
+    {
+        "courseName":"Complete Sql Mastery",
+        "instructor":"Mosh Hamedani",
+        "duration":"2 Months",
+        "picture":"https://i.ibb.co/m4SdkdD/courses-online-2.jpg",
+        "id":3,
+        "price":47
+  
+      },
+
+      {
+        "courseName":"Angular:Beginner To Pro",
+        "instructor":"Mosh Hamedani",
+        "duration":"4 Months",
+        "picture":"https://i.ibb.co/5FDPkjQ/courses-online-2.png",
+        "id":4,
+        "price":110
+  
+      },
+
+  
+      {
+        "courseName":"Clean Coding And Refactoring",
+        "instructor":"Mosh Hamedani",
+        "duration":"4 Months",
+        "picture":"https://i.ibb.co/mRP40WB/courses-online-3.jpg",
+        "id":5,
+        "price":118
+  
+      },
+
+      
+      {
+        "courseName":"The Ultimate React Native Series",
+        "instructor":"Mosh Hamedani",
+        "duration":"5 Months",
+        "picture":"https://i.ibb.co/zhcs8Pd/courses-online-3.png",
+        "id":6,
+        "price":140
+  
+      },
+      {
+        "courseName":"The Complete Node.js Series",
+        "instructor":"Mosh Hamedani",
+        "duration":"3 Months",
+        "picture":"https://i.ibb.co/307yFC1/courses-online-4.jpg",
+        "id":7,
+        "price":150
+  
+      },
+
+      {
+        "courseName":"Redux in Angular",
+        "instructor":"Mosh Hamedani",
+        "duration":"4 Months",
+        "picture":"https://i.ibb.co/M6dRG91/courses-online-4.png",
+        "id":8,
+        "price":160
+  
+      },
+
+      {
+        "courseName":"The Complete ASP.Net ",
+        "instructor":"Mosh Hamedani",
+        "duration":"4 Months",
+        "picture":"https://i.ibb.co/QHgB4KN/courses-online-5.jpg",
+        "id":9,
+        "price":170
+  
+      },
+      
+
+
+      {
+        "courseName":"Entity Framework 6 in Depth",
+        "instructor":"Mosh Hamedani",
+        "duration":"8 Months",
+        "picture":"https://i.ibb.co/wK2KBGB/courses-online-5.png",
+        "id":10,
+        "price":180
+  
+      },
+      {
+        "courseName":"The Ultimate Java Mastery Series",
+        "instructor":"Mosh Hamedani",
+        "duration":"2 Months",
+        "picture":"https://i.ibb.co/Fg2QKkB/courses-online-6.jpg",
+        "id":11,
+        "price":210
+  
+      },
+
+      {
+        "courseName":"Complete Python Mastery",
+        "instructor":"Mosh Hamedani",
+        "duration":"4 Months",
+        "picture":"https://i.ibb.co/QCvcMpK/courses-online-7.jpg",
+        "id":12,
+        "price":170
+  
+      },
+
+      {
+        "courseName":"Unit Testing",
+        "instructor":"Mosh Hamedani",
+        "duration":"4 Months",
+        "picture":"https://i.ibb.co/zb2RNy0/courses-online-8.jpg",
+        "id":13,
+        "price":270
+  
+      },
+      
+
+      {
+        "courseName":"Flutter in a month",
+        "instructor":"Mosh Hamedani",
+        "duration":"1 Months",
+        "picture":"https://i.ibb.co/8zbNC17/courses-online-9.jpg",
+        "id":14,
+        "price":250
+  
+      },
+
+      {
+        "courseName":"Become a full-Stack web Developer",
+        "instructor":"Mosh Hamedani",
+        "duration":"3 Months",
+        "picture":"https://i.ibb.co/CK5xM2j/courses-online-10.jpg",
+        "id":15,
+        "price":"270"
+  
+      }
+    ]
+      export default data;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
